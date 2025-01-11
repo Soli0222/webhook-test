678b72923fe0ea70883ef676f766003f9fa367c3
Soli0222 <github@str08.net>
2025-01-12 01:27:06 +0900

    enhance webhook logging to include headers and pretty-print body

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
@@ -12,13 +13,36 @@ func webhookHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	headers := make(map[string][]string)
+	for key, values := range r.Header {
+		headers[key] = values
+	}
+
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
 		return
 	}
 
-	log.Println("Received webhook:", string(body))
+	headersJSON, err := json.MarshalIndent(headers, "", "  ")
+	if err != nil {
+		log.Println("Failed to marshal headers:", err)
+	}
+
+	var prettyBody string
+	var jsonBody interface{}
+	if err := json.Unmarshal(body, &jsonBody); err != nil {
+		prettyBody = string(body)
+	} else {
+		prettyBodyBytes, err := json.MarshalIndent(jsonBody, "", "  ")
+		if err != nil {
+			prettyBody = string(body)
+		} else {
+			prettyBody = string(prettyBodyBytes)
+		}
+	}
+
+	log.Printf("Received webhook:\nHeaders:\n%s\nBody:\n%s\n", headersJSON, prettyBody)
 	w.WriteHeader(http.StatusOK)
 }
 
